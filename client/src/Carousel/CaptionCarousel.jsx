267169213944bc0e7829e38e78581703d858b04f
MariFuhr <mariarosafuhr@gmail.com>
2023-09-21 00:06:39 -0300

    landing casi lista

--- client/src/Carousel/CaptionCarousel.jsx
+++ client/src/Carousel/CaptionCarousel.jsx
@@ -39,8 +39,8 @@ export default function CaptionCarousel() {
     {
       title: 'Design Projects 1',
       text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-      image:
-        'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+     image: 'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        
     },
     {
       title: 'Design Projects 2',
@@ -95,35 +95,41 @@ export default function CaptionCarousel() {
       </IconButton>
       {/* Slider */}
       <Slider {...settings} ref={(slider) => setSlider(slider)}>
-        {cards.map((card, index) => (
-          <Box
-            key={index}
-            height={'6xl'}
-            position="relative"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            backgroundImage={`url(${card.image})`}>
-            {/* This is the block you need to change, to customize the caption */}
-            <Container size="container.lg" height="600px" position="relative">
-              <Stack
-                spacing={6}
-                w={'full'}
-                maxW={'lg'}
-                position="absolute"
-                top="50%"
-                transform="translate(0, -50%)">
-                <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                  {card.title}
-                </Heading>
-                <Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText">
-                  {card.text}
-                </Text>
-              </Stack>
-            </Container>
-          </Box>
-        ))}
-      </Slider>
+  {cards.map((card, index) => (
+    <Box
+      key={index}
+      height={'600px'}
+      position="relative"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      backgroundImage={`url(${card.image})`}
+    >
+      <Container size="container.lg" height="600px" position="relative">
+        <Stack
+          spacing={6}
+          w={'full'}
+          maxW={'lg'}
+          position="absolute"
+          top="50%"
+          transform="translate(0, -50%)"
+        >
+          <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+            {card.title}
+          </Heading>
+          <img
+            src={card.image}
+            alt={card.title}
+            style={{ maxWidth: '100%', height: 'auto' }} // Estilos para ajustar el tamaño
+          />
+          <Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText">
+            {card.text}
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  ))}
+</Slider>
     </Box>
   )
 }
--- client/src/LandingPage/LandingPage.jsx
+++ client/src/LandingPage/LandingPage.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
-import CaptionCarousel from '../Carousel/CaptionCarousel'
-
-
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import NavBar from '../NavBar/NavBar';
+import CaptionCarousel from '../Carousel/CaptionCarousel';
 
 export default function LandingPage() {
   return (
     <div>
-        <NavBar />
+      <NavBar />
+      <div>
+        <div style={{ marginTop: '40px' }}>
+          {/* Aquí puedes agregar cualquier otro contenido adicional */}
+        </div>
         <CaptionCarousel />
+      </div>
+      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+        <div className="flex items-center justify-center">
+          <a href="https://facebook.com">
+            <FontAwesomeIcon icon={faFacebook} className="icon" />
+          </a>
+          <a href="https://twitter.com" >
+            <FontAwesomeIcon icon={faTwitter} className="icon" />
+          </a>
+          <a href="https://instagram.com">
+            <FontAwesomeIcon icon={faInstagram} className="icon" />
+          </a>
+        </div>
+        <p className="ml-2">Copyright © 2023 - All right reserved</p>
+      </footer>
     </div>
-  )
+  );
 }
